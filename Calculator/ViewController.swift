fe0881357bc4b71e6e5cb39c92c536639f9933ae
Kylta <Christophe.bugnon02@gmail.com>
2018-02-02 15:43:01 +0100

    create calculate variable displayValue and refactor performOperation method

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -17,11 +17,19 @@ class ViewController: UIViewController {
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDisplay = display!.text!
-            display!.text = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
         } else {
             display.text = digit
             userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        } set {
+            display.text = String(newValue)
         }
     }
     
@@ -30,10 +38,9 @@ class ViewController: UIViewController {
         if let mathematicalSymbol = sender.currentTitle {
             switch mathematicalSymbol {
             case "π":
-                display!.text = String(Double.pi)
+                displayValue = Double.pi
             case "√":
-                let operand = Double(display!.text!)!
-                display!.text = String(sqrt(operand))
+                displayValue = sqrt(displayValue)
             default:
                 break
             }
