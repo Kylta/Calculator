2c978e3103d3420a0acb7d94b768096e283f9a39
Kylta <Christophe.bugnon02@gmail.com>
2018-02-05 10:14:12 +0100

    create Model files with CalculatorBrain, create method for perform operation, set operation and get result from these operation. Add it in ViewController

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,37 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Christophe Bugnon on 05/02/2018.
+//  Copyright © 2018 Christophe Bugnon. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    
+    mutating func performOperation(_ symbol: String) {
+        switch symbol {
+        case "π":
+            accumulator = Double.pi
+        case "√":
+            if let operand = accumulator {
+            accumulator = sqrt(operand)
+            }
+        default:
+            break
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+}
